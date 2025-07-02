b140027c2740ade0eefb8c9c6a9a193ca55fd61d
Кормилицын Илья Сергеевич <kormilitsyn.ilya@mail.ru>
2025-07-02 23:15:21 +0700

    Change structure main jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./scss/app.scss";
+
+import MainPage from "./pages/MainPage";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <MainPage></MainPage>
+  </>
+);
